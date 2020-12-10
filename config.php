f129cb6e8b7377252841e520f9e1fea63e042d40
Philona Reetha Sebastian <60103472+philona123@users.noreply.github.com>
2020-12-10 14:08:05 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,12 @@
 <?php
 
-	$conn = mysqli_connect('sql207.epizy.com','epiz_27374481','Qp69efG3NNV3','epiz_27374481_sparksbank');
+	$servername = "localhost";
+        $username = "id15598478_sparks";
+        $password = "LV_unbg9OyM}oCXE";
+        $database = "id15598478_sparkbank";
+
+// Create connection
+        $conn = mysqli_connect($servername, $username, $password, $database);
 
 	if(!$conn){
 		die("Unable to connect to the database due to the following error --> ".mysqli_connect_error());
